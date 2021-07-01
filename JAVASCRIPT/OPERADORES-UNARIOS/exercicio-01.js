3b5f620f3654ff6793461b55ccd5d28938d6c465
Larvin <49764430+Larvin-Vinicius@users.noreply.github.com>
2021-06-30 22:44:27 -0300

    Exercicio 01 de unarios

---
+++ JAVASCRIPT/OPERADORES-UNARIOS/exercicio-01.js
@@ -0,0 +1,18 @@
+// https://javascript.info/task/increment-order
+// Quais os respectivos valores de cada variável?
+
+let a = 1;
+let b = 1;
+
+let c = ++a;
+let d = b++;
+
+/*
+Resposta
+
+a = 1
+b = 1
+c = 2
+d = 1
+*/
+
