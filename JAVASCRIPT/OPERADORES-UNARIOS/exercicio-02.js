32f129b65ae819dce2b62c925e6af4d308d4d054
Larvin <49764430+Larvin-Vinicius@users.noreply.github.com>
2021-06-30 22:44:42 -0300

    Exercicio 02 de unarios

---
+++ JAVASCRIPT/OPERADORES-UNARIOS/exercicio-02.js
@@ -0,0 +1,9 @@
+// Convertendo valores de entrada.
+
+let a = +prompt("Insira a quantidade de maçãs");
+let b = +prompt("Insira a quantidade de laranjas");
+let c = +prompt("Insira a quantidade de abacaxis");
+
+let totalDeFrutas = a + b + c;
+
+alert(` O numero total de frutas é ${totalDeFrutas}`);
