3cd1b65a7e3859d959c6452056d88510fc89de64
Larvin <49764430+Larvin-Vinicius@users.noreply.github.com>
2022-01-12 10:04:52 -0300

    Delete JAVASCRIPT/OPERADORES-UNARIOS directory

--- JAVASCRIPT/OPERADORES-UNARIOS/exercicio-02.js
+++
@@ -1,9 +0,0 @@
-// Convertendo valores de entrada.
-
-let a = +prompt("Insira a quantidade de maçãs");
-let b = +prompt("Insira a quantidade de laranjas");
-let c = +prompt("Insira a quantidade de abacaxis");
-
-let totalDeFrutas = a + b + c;
-
-alert(` O numero total de frutas é ${totalDeFrutas}`);
